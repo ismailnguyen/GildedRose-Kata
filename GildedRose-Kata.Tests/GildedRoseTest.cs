c6de7d51099bb584fdbd58eb54c0dd22caa16188
Ismaïl <nguyen.ismail@gmail.com>
2017-11-09 14:34:52 +0100

    Add test for backstage with sellin under 11

--- GildedRose-Kata.Tests/GildedRoseTest.cs
+++ GildedRose-Kata.Tests/GildedRoseTest.cs
@@ -8,7 +8,8 @@ namespace GildedRose_Kata.Tests
     {
         [TestCase("Axe", 23, 17, 22, 16)]
         [TestCase("Aged Brie", 23, 21, 22, 22)]
-        [TestCase("Backstage passes to a TAFKAL80ETC concerts", 23, 21, 22, 20)]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 23, 21, 22, 22)]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 10, 21, 9, 23)]
         public void Should(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
         {
             // GIVEN
