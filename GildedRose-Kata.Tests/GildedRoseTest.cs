28434c2129cc9a8519ba9aff8979f575c8ab569b
Ismaïl <nguyen.ismail@gmail.com>
2017-11-09 13:46:21 +0100

    Initialize empty poject

---
+++ GildedRose-Kata.Tests/GildedRoseTest.cs
@@ -0,0 +1,30 @@
+﻿using NFluent;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace GildedRose_Kata.Tests
+{
+    public class GildedRoseTest
+    {
+        [TestCase("Axe", 23, 17)]
+        public void Should(string name, int sellIn, int quality)
+        {
+            // GIVEN
+            Item axe = new Item()
+            {
+                Name = name,
+                SellIn = sellIn,
+                Quality = quality
+            };
+
+            GildedRose ap = new GildedRose(new List<Item> { axe });
+
+            // WHEN
+            ap.UpdateQuality();
+
+            // THEN
+            Check.That(axe.SellIn).IsEqualTo(22);
+            Check.That(axe.Quality).IsEqualTo(16);
+        }
+    }
+}
