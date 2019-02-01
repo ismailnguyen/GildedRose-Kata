500bd2d7ef015aaa019601220ebe2bae02c2ae75
Ismaïl NGUYEN <ismail.nguyen@external.loccitane.com>
2019-02-01 15:58:14 +0100

    [REFACTOR] Extract item class to separate file

---
+++ GildedRose-Kata/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace GildedRose_Kata
+{
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int SellIn { get; set; }
+
+        public int Quality { get; set; }
+    }
+}
